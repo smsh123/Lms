9e49cc51deb086c3947e08ae1677b9182630821d
“Vivek <vivekkumar.sharma@safalta.com>
2023-10-11 21:59:08 +0530

    js link changed

--- resources/views/cms/layouts/master.blade.php
+++ resources/views/cms/layouts/master.blade.php
@@ -122,7 +122,7 @@
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
       <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
-      <script src="../assets/js/bootstrap.bundle.min.js"></script>
+      <script src="/assets/js/bootstrap.bundle.min.js"></script>
 
       
         <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js"></script>

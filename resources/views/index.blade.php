f5011ea7512d37d04f7d5a86ac1e20caeda83781
“Vivek <vivekkumar.sharma@safalta.com>
2023-10-13 13:27:48 +0530

    fix header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 @extends('cms.layouts.master')
 @section('body')
-  @include('v3.layouts.global_header')
+  @include('layouts.global_header')
     <div class="container-fluid">
         <h1>Welcome to AryaBhatt Classes</h1>
     </div>

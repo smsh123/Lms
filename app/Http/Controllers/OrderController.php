4b11d726b70112947eedf7326b3421f4e0651086
Somesh <somesh.kumar@safalta.com>
2023-12-09 16:09:13 +0530

    test ci cd 123456

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,7 +15,7 @@ class OrderController extends Controller
 {
     //
     public function index(Request $request){
-        // test
+        // test 123
         $orders = Order::all();
         return view('cms.orders.index')->with('orders',$orders);
     }

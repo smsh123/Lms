bda94ed325bae68978b4172053fb442ef823a469
“Vivek <vivekkumar.sharma@safalta.com>
2023-12-30 21:36:50 +0530

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,9 +46,9 @@ class UserController extends Controller
     }
     public function edit(Request $request, $id)
     {
-        if(!in_array('Edit User',\Auth::user()->permissions)){
-            return redirect()->back()->with('error', 'Permission Denied');
-        }
+        // if(!in_array('Edit User',\Auth::user()->permissions)){
+        //     return redirect()->back()->with('error', 'Permission Denied');
+        // }
         $users = User::find($id);
         $roles = Role::all(['name']);
         $roles = is_object($roles) ? $roles->toArray() : $roles;

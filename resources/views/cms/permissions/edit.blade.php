596acee6da1fcd0b0963b8132b7886b369cfb510
“Vivek <vivekkumar.sharma@safalta.com>
2023-12-30 20:35:38 +0530

    fix permission

--- resources/views/cms/permissions/edit.blade.php
+++ resources/views/cms/permissions/edit.blade.php
@@ -1,8 +1,8 @@
 @extends('cms.layouts.master')
 @section('body')
   <div class="row my-3">
-    <div class="col-12 col-lg-6"><h1>Edit Roles</h1></div>
-    <div class="col-12 col-lg-6 text-right"><a href="/cms/roles" class="btn btn-lg btn-secondary">View Roles</a></div>
+    <div class="col-12 col-lg-6"><h1>Edit Permission</h1></div>
+    <div class="col-12 col-lg-6 text-right"><a href="/cms/permissions" class="btn btn-lg btn-secondary">View Permissions</a></div>
   </div>
 
 
